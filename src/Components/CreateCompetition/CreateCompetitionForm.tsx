5f0237066c2ac11750b44bbc0f4e7ce235294c27
Kuba <kuba.nowakowski@sabre.com>
2022-07-17 14:06:12 +0200

    pages to creation competition from

--- src/Components/CreateCompetition/CreateCompetitionForm.tsx
+++ src/Components/CreateCompetition/CreateCompetitionForm.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import {Button, Col, Form, Row} from "react-bootstrap";
+import Category from "../../Objects/Category";
 
 
 const CreateCompetitionsFirstPage = () => {
+    const [page, setPage] = useState<Number>(0);
 
 
-    return (
-        <>
-        </>
-    )
+    switch (page){
+        case 0:
+            return (
+                <>
+                    <CreateCompetitionsFirstPage/>
+                </>)
+        break;
+    }
 }
 
 export default CreateCompetitionsFirstPage;
